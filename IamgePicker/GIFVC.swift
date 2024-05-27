3fb63d02b506984dec390021400d89f811bed25b
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-27 15:42:57 -0700

    ommit

---
+++ IamgePicker/GIFVC.swift
@@ -0,0 +1,29 @@
+//
+//  GIFVC.swift
+//  IamgePicker
+//
+//  Created by Arpit iOS Dev. on 27/05/24.
+//
+
+import UIKit
+
+class GIFVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IamgePicker/RatingBar.swift
@@ -0,0 +1,20 @@
+//
+//  RatingBar.swift
+//  IamgePicker
+//
+//  Created by Arpit iOS Dev. on 27/05/24.
+//
+
+import UIKit
+
+class RatingBar: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
